92416437df9e01101eb6d570002d8550c9db4390
Gustavo Kureck Mendes <gustavo@portabilis.com.br>
2021-03-10 22:26:37 -0300

    Importa countdownProvider e melhora indentaçao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
 import { ChallengeBox } from '../components/ChallengeBox'
+import { CountdownProvider } from '../contexts/CountdownContext'
 
 function App() {
   return (
@@ -15,17 +16,18 @@ function App() {
         <title>Inicio | PomodoroTime</title>
       </Head>
       <ExperienceBar />
-
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <CountDown />
-        </div>
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <CountDown />
+          </div>
+          <div>
+            <ChallengeBox/>
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
