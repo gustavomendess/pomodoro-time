7a0adb0673357fe4d36b08f2a2f41e4901656014
gustavomendess <gustavocod13@outlook.com>
2021-05-12 23:09:39 -0300

    Remove uso de foto e nome

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,10 +5,6 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/gustavomendess.png" alt="Gustavo Mendes"/>
-      <div>
-        <strong>Gustavo Mendes</strong>
-      </div>
     </div>
   )
 }
